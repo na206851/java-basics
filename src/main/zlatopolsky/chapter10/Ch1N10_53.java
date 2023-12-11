b7aa85bb71dcf1a458ee0d00771c561929d66359
Nikita <na206851@gmail.com>
2023-12-11 16:27:23 +0800

    corrected name

--- src/main/zlatopolsky/chapter10/Ch1N10_53.java
+++ src/main/zlatopolsky/chapter10/Ch1N10_53.java
@@ -7,10 +7,10 @@ public class Ch1N10_53 {
     /**
      * this method returns the reverse sequence of numbers until zero is entered
      */
-    public void reserseSequenceOfNumber(Scanner sc) {
+    public void reverseSequenceOfNumber(Scanner sc) {
         int number = sc.nextInt();
         if (number != 0) {
-            reserseSequenceOfNumber(sc);
+            reverseSequenceOfNumber(sc);
             System.out.println(number);
         }
     }
