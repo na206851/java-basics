5deadcd0c84de2f7377e0f2ba78a6f84f3e3604c
Nikita <na206851@gmail.com>
2023-12-12 14:24:42 +0800

    fix Ch1N10_43 : rename method and fix error

--- src/main/zlatopolsky/chapter10/Ch1N10_43.java
+++ src/main/zlatopolsky/chapter10/Ch1N10_43.java
@@ -2,17 +2,17 @@ package zlatopolsky.chapter10;
 
 public class Ch1N10_43 {
 
-    public static int sumDigits(int x) {
+    public int sumDigits(int x) {
         if (x <= 0) {
             return x;
         }
         return x % 10 + (sumDigits(x / 10));
     }
 
-    public static int sumDigits(int x, int count) {
-        if (x <= 0) {
-            return x;
+    public int countDigits(int x) {
+        if (x <= 10) {
+            return 1;
         }
-        return x % 10 + sumDigits(x / 10);
+        return 1 + countDigits(x / 10);
     }
 }
