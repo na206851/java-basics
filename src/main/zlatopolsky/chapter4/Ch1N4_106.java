f1f6421d48448fc934064f13d052f9f01c25b73f
Nikita <na206851@gmail.com>
2023-12-13 12:52:10 +0800

    delete case 1, 2, 12

--- src/main/zlatopolsky/chapter4/Ch1N4_106.java
+++ src/main/zlatopolsky/chapter4/Ch1N4_106.java
@@ -6,23 +6,19 @@ public class Ch1N4_106 {
         String season = "";
         if (month > 12) {
             month %= 12;
-        } else {
-            switch (month) {
-                case 12, 1, 2:
-                    season = "winter";
-                    break;
-                case 3, 4, 5:
-                    season = "spring";
-                    break;
-                case 6, 7, 8:
-                    season = "summer";
-                    break;
-                case 9, 10, 11:
-                    season = "autumn";
-                    break;
-                default:
-                    season = "winter";
-            }
+        }
+        switch (month) {
+            case 3, 4, 5:
+                season = "spring";
+                break;
+            case 6, 7, 8:
+                season = "summer";
+                break;
+            case 9, 10, 11:
+                season = "autumn";
+                break;
+            default:
+                season = "winter";
         }
         return season;
     }
--- src/test/zlatopolsky/chapter4/Ch1N4_106Test.java
+++ src/test/zlatopolsky/chapter4/Ch1N4_106Test.java
@@ -3,7 +3,7 @@ package zlatopolsky.chapter4;
 class Ch1N4_106Test {
     public static void main(String[] args) {
         Ch1N4_106 test = new Ch1N4_106();
-        for (int i = 1; i <= 12; i++) {
+        for (int i = 10; i <= 20; i++) {
             System.out.println(test.season(i));
         }
     }
