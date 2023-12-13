43a9c1361acd594cf2a0361b7a209dc6fc6cf74a
Nikita <na206851@gmail.com>
2023-12-13 12:11:42 +0800

    tested method

--- src/test/zlatopolsky/chapter10/Ch1N10_52Test.java
+++ src/test/zlatopolsky/chapter10/Ch1N10_52Test.java
@@ -3,6 +3,6 @@ package zlatopolsky.chapter10;
 class Ch1N10_52Test {
     public static void main(String[] args) {
         Ch1N10_52 test = new Ch1N10_52();
-        System.out.println(test.reverseNumber(123));
+        test.reverseNumber(123);
     }
 }
