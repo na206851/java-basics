f306a2dcd19a4125a88982e146850330fb238c2d
Nikita <na206851@gmail.com>
2023-12-18 16:03:08 +0800

    fix Ch1N10_52 reverse method

--- src/java/main/zlatopolsky/chapter10/Ch1N10_52.java
+++ src/java/main/zlatopolsky/chapter10/Ch1N10_52.java
@@ -2,11 +2,12 @@ package zlatopolsky.chapter10;
 
 public class Ch1N10_52 {
 
-    public int reverseNumber(int n) {
-        if (n < 10) {
-            return n;
-        } else {
-            return Integer.parseInt(String.valueOf(n % 10) + reverseNumber(n / 10));
+    public int reverseNumber(int number, int reverse) {
+        if (number == 0) {
+            return reverse;
         }
+
+        reverse = (reverse * 10) + (number % 10);
+        return reverseNumber(number / 10, reverse);
     }
 }
--- src/java/test/zlatopolsky/chapter10/Ch1N10_52Test.java
+++ src/java/test/zlatopolsky/chapter10/Ch1N10_52Test.java
@@ -3,6 +3,7 @@ package zlatopolsky.chapter10;
 class Ch1N10_52Test {
     public static void main(String[] args) {
         Ch1N10_52 test = new Ch1N10_52();
-        test.reverseNumber(123);
+        System.out.println(test.reverseNumber(23, 0));
+        System.out.println(test.reverseNumber(0, 0));
     }
 }
