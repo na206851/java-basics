61bb5a27f31b5f21863855e52093febea313a42b
Nikita <na206851@gmail.com>
2023-12-20 14:25:46 +0800

    rename method

--- src/java/main/zlatopolsky/chapter10/Ch1N10_50.java
+++ src/java/main/zlatopolsky/chapter10/Ch1N10_50.java
@@ -2,13 +2,13 @@ package zlatopolsky.chapter10;
 
 public class Ch1N10_50 {
 
-    public int accerman(int n, int m) {
+    public int functionAccerman(int n, int m) {
         if (n == 0) {
             return m + 1;
         } else if (n != 0 && m == 0) {
-            return accerman(n - 1, 1);
+            return functionAccerman(n - 1, 1);
         } else if (n > 0 && m > 0) {
-            return accerman(n - 1, accerman(n, m - 1));
+            return functionAccerman(n - 1, functionAccerman(n, m - 1));
         } else {
             return 0;
         }
--- src/java/test/zlatopolsky/chapter10/Ch1N10_50Test.java
+++ src/java/test/zlatopolsky/chapter10/Ch1N10_50Test.java
@@ -3,6 +3,6 @@ package zlatopolsky.chapter10;
 class Ch1N10_50Test {
     public static void main(String[] args) {
         Ch1N10_50 test = new Ch1N10_50();
-        System.out.println(test.accerman(4, 5));
+        System.out.println(test.functionAccerman(4, 5));
     }
 }
