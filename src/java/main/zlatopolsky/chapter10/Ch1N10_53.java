4090f4ccd80fb2ee277e3645ede583563c840fca
Nikita <na206851@gmail.com>
2023-12-23 18:19:04 +0800

    remove comment

--- src/java/main/zlatopolsky/chapter10/Ch1N10_53.java
+++ src/java/main/zlatopolsky/chapter10/Ch1N10_53.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Ch1N10_53 {
 
-    /**
-     * this method returns the reverse sequence of numbers until zero is entered
-     */
     public void reverseSequenceOfNumber(Scanner sc) {
         int number = sc.nextInt();
         if (number != 0) {
