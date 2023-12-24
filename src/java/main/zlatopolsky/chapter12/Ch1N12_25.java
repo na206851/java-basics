8ce56e7ede11a5b5a07d7549dad14b8b0fa8f971
Nikita <na206851@gmail.com>
2023-12-24 18:37:27 +0800

    tested method and method print static

--- src/java/main/zlatopolsky/chapter12/Ch1N12_25.java
+++ src/java/main/zlatopolsky/chapter12/Ch1N12_25.java
@@ -237,9 +237,9 @@ public class Ch1N12_25 {
         return matrix;
     }
 
-    public void print(int[][] matrix) {
-        for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < 10; j++) {
+    public static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
--- src/java/test/zlatopolsky/chapter12/Ch1N12_28Test.java
+++ src/java/test/zlatopolsky/chapter12/Ch1N12_28Test.java
@@ -1,11 +1,14 @@
 package zlatopolsky.chapter12;
 
-import java.util.Arrays;
+import static zlatopolsky.chapter12.Ch1N12_25.print;
 
 class Ch1N12_28Test {
     public static void main(String[] args) {
         Ch1N12_28 test = new Ch1N12_28();
-        System.out.println(Arrays.deepToString(test.spiralMatrix(6, 6)));
-        System.out.println(Arrays.deepToString(test.spiralMatrix(6, 5)));
+
+        print(test.spiralMatrix(6, 6));
+        print(test.spiralMatrix(6, 5));
+        print(test.spiralMatrix(2, 2));
+        print(test.spiralMatrix(2, 3));
     }
 }
